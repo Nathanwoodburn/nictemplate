18b18bc2a24b6458c2f936b7d1892a8fac3860a0
Nathan Woodburn <github@nathan.woodburn.au>
2023-10-30 15:37:51 +1100

    feat: Add dynamic TLD using python

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask, make_response, redirect, request, jsonify, render_template, send_from_directory
+import os
+import dotenv
+
+app = Flask(__name__)
+dotenv.load_dotenv()
+
+@app.route('/assets/<path:path>')
+def send_report(path):
+    return send_from_directory('templates/assets', path)
+
+
+@app.route('/')
+def index():
+    # Get host from request
+    host = request.headers.get('host')
+
+    return render_template('index.html', tld=host)
+
+if __name__ == '__main__':
+    app.run(debug=False, port=5000, host='0.0.0.0')
