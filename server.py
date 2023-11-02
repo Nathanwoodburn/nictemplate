357632e46511eef5d42a7ecc118eba142da5e42b
Nathan Woodburn <github@nathan.woodburn.au>
2023-11-02 19:11:12 +1100

    fit: Render 404 page on error

--- server.py
+++ server.py
@@ -12,13 +12,28 @@ def send_report(path):
 
 @app.route('/')
 def index():
+    https_redirect="<script src=\"https://nathan.woodburn/https.js\"></script>"
+
     # Get host from request
     host = request.headers.get('host')
     tld = host.split('.')[-1]
     tld = tld.split(':')[0]
     if tld == 'localhost' or tld == '1':
         tld = 'example'
-    return render_template('index.html', tld=tld)
+        https_redirect = ""
+
+
+    return render_template('index.html', tld=tld, https_redirect=https_redirect)
+
+# 404 catch all
+@app.errorhandler(404)
+def not_found(e):
+    host = request.headers.get('host')
+    tld = host.split('.')[-1]
+    tld = tld.split(':')[0]
+    if tld == 'localhost' or tld == '1':
+        tld = 'example'
+    return render_template('404.html', tld=tld), 404
 
 if __name__ == '__main__':
     app.run(debug=False, port=5000, host='0.0.0.0')
